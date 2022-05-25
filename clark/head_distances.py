360aa781a419640c7d44fd4668586c18f011dae6
Elisabetta Rocchetti <elisabetta.rocchetti0@gmail.com>
2022-05-25 15:58:47 +0200

    attention update

--- clark/head_distances.py
+++ clark/head_distances.py
@@ -13,20 +13,27 @@ def main():
     help="Pickle file containing extracted attention maps.")
   parser.add_argument("--outfile", required=True,
                       help="Where to write out the distances between heads.")
+  parser.add_argument(
+    "--n-layers", required=True,
+    help="Number of layers in transormer.")
+  parser.add_argument(
+    "--n-heads", required=True,
+    help="Number of heads in transormer layer.")
   args = parser.parse_args()
 
   print("Loading attention data")
   data = utils.load_pickle(args.attn_data_file)
 
   print("Computing head distances")
-  js_distances = np.zeros([144, 144])
+  n = int(args.n_layers) * int(args.n_heads)
+  js_distances = np.zeros([n, n])
   for doc in utils.logged_loop(data, n_steps=None):
     if "attns" not in doc:
       continue
     tokens, attns = doc["tokens"], np.array(doc["attns"])
-
-    attns_flat = attns.reshape([144, attns.shape[2], attns.shape[3]])
-    for head in range(144):
+    
+    attns_flat = attns.reshape([n, attns.shape[2], attns.shape[3]])
+    for head in range(n):
       head_attns = np.expand_dims(attns_flat[head], 0)
       head_attns_smoothed = (0.001 / head_attns.shape[1]) + (head_attns * 0.999)
       attns_flat_smoothed = (0.001 / attns_flat.shape[1]) + (attns_flat * 0.999)
